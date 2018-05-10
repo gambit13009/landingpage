253449281072cc5bd0335a5a86216b1726a48ce5
sam13008 <ikherbane.samir@gmail.com>
2018-05-10 11:20:29 +0200

    rooting

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
+	
+	root "users#new"
+	resources :users, only: [:new, :create]
+
+                        
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
